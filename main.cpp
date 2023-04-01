387b9b3cbac9b44e8d5dfdac9b7e4560dd8cc70b
soumit-s <soumit.srim@gmail.com>
2023-04-01 16:45:18 +0530

    ...

--- main.cpp
+++ main.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
 #include "gui.h"
+#include "app.h"
 
-int main() {
-	Window wnd("Hello :)", 800, 600);
+int old() {
+	try {
+		Window wnd("Hello :)", 800, 600);
 
-	bool quit = false;
+		bool quit = false;
 
-	auto ehandler = EventHandler([&quit](Event* e) {
-		if (e->type == EventType::QUIT)
-			quit = true;
-		});
+		auto ehandler = EventHandler([&quit](Event* e) {
+			if (e->type == EventType::QUIT)
+				quit = true;
+			});
 
-	wnd.set_event_handler(&ehandler);
+		wnd.set_event_handler(&ehandler);
 
-	std::cout << wnd.scan_num_joysticks() << std::endl;
-	
-	if (!wnd.select_controller_device()) {
-		throw std::runtime_error("Failed to select controller");
+		std::cout << wnd.scan_num_joysticks() << std::endl;
+
+		if (!wnd.select_controller_device()) {
+			throw std::runtime_error("Failed to select controller");
+		}
+
+		while (!quit) {
+			wnd.handle_events();
+		}
+
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
 	}
-	
-	while (!quit) {
-		wnd.handle_events();
+
+	return 0;
+}
+
+int main () {
+	try {
+		App app;
+		app.start();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
 	}
 
 	return 0;
