8bbb72c796ce2f62fe8ae3b3ecde5a4af992d397
soumit-s <soumit.srim@gmail.com>
2023-04-02 17:38:52 +0530

    Added protocol macro definitions for various instructions.

--- app.h
+++ app.h
@@ -1,8 +1,21 @@
 #pragma once
 #include "gui.h"
+#include "serial.h"
+
+
+#define PROTOCOL_MOVE_FORWARD 1
+#define PROTOCOL_STOP 0
+#define PROTOCOL_MOVE_BACKWARD 2
+
+#define PROTOCOL_MOVE_LEFT 3
+#define PROTOCOL_MOVE_STRAIGHT 4
+#define PROTOCOL_MOVE_RIGHT 5
+
 class App {
 	Window* wnd;
 	EventHandler* ehandler;
+
+	Serial* serial;
 
 	bool is_quit;
 public:
