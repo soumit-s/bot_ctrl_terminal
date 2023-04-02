25adeb477c7c1cdb71fb81e6641cc2cc360e8555
Sampurna Dan <dansampurna@gmail.com>
2023-04-02 14:43:57 +0530

    Added configure() function. Changed the return types of open(), write(), close() to boolean.

--- serial.h
+++ serial.h
@@ -5,15 +5,19 @@
 class Serial {
 	std::wstring port_name;
 	int baud_rate;
+	HANDLE port;
 public:
 	Serial(const std::wstring& port_name, int baud_rate);
 
 	// Opens the serial port for reading and writing.
-	void open();
+	bool open();
+
+	//Restoring and modifying configurations
+	bool configure();
 
 	// Writes a byte to the serial port.
-	void write(uint8_t b);
+	bool write(uint8_t b);
 
 	// Closes the serial port.
-	void close();
+	bool close();
 };
